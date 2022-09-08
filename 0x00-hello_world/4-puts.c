74ee69d3fb30665be0004a207b7c09637b8fb368
JOHNSONQUAQUE <quaquejohn10@yahoo.com>
2022-09-08 14:44:27 -0700

    updated commmit message

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - Entry point
@@ -9,13 +8,9 @@
  *    * Return: always 0
  *
  *     */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
 
 }
