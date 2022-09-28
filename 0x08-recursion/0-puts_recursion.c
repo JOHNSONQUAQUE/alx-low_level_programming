5b6e6437854ce7ab5547796803ace7e5e2bba5e3
JOHNSONQUAQUE <quaquejohn10@yahoo.com>
2022-09-28 15:34:51 -0700

    updated commit message

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,20 +1,21 @@
-#include "main.h"
+/*
+ * File: 0-puts_recursion.c
+ * Auth: sam
+ */
 
+#include "main.h"
 /**
- * _puts_recursion - prints's a string followed by a new line
- * @s: string to be printed
- *
- * Return: void
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		_putcchar('\n');
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
+
 	else
-	{
-		_putchar(s[0]);
-		_puts_recursion(s +1);
-	}
+		_putchar('\n');
 }
